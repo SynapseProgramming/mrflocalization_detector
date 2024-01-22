41fb8f7a8b313674f0cbc2fae908fe3c7cb12a90
Roald Ong <roald.ong@gmail.com>
2024-01-22 10:43:50 +0800

    Add in distance map generation function

--- include/mrflocalization_detector/mrf.h
+++ include/mrflocalization_detector/mrf.h
@@ -4,11 +4,14 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <opencv2/opencv.hpp>
 #include <ros/ros.h>
 #include <tf2/LinearMath/Transform.h>
 #include <tf2_ros/buffer.h>
 #include <geometry_msgs/PointStamped.h>
 #include <sensor_msgs/LaserScan.h>
+#include <mrflocalization_detector/Pose.h>
+#include <nav_msgs/OccupancyGrid.h>
 
 enum MeasurementClass
 {
@@ -20,19 +23,30 @@ enum MeasurementClass
 class MRF
 {
 private:
+    // map
+    cv::Mat distMap_;
+    double mapResolution_;
+    Pose mapOrigin_;
+    int mapWidth_, mapHeight_;
+    bool gotMap_;
+
     // ros subscribers and publishers
     ros::NodeHandle nh_;
     ros::Subscriber scanSub_;
+    ros::Subscriber mapSub_;
 
     // tf2_ros::Buffer tfBuffer;
 
-
+    // callback function definitions
     void scanCB(const sensor_msgs::LaserScan::ConstPtr &msg);
+    void mapCB(const nav_msgs::OccupancyGrid::ConstPtr &msg);
 
 public:
     MRF() : nh_("~")
     {
+        // TODO: topic names should be parameters
         scanSub_ = nh_.subscribe("/scan_front", 1, &MRF::scanCB, this);
+        mapSub_ = nh_.subscribe("/map", 1, &MRF::mapCB, this);
     }
 };
 
--- src/mrf.cpp
+++ src/mrf.cpp
@@ -5,3 +5,46 @@ void MRF::scanCB(const sensor_msgs::LaserScan::ConstPtr &msg)
 
     std::cout << "scan message received!\n";
 }
+
+void MRF::mapCB(const nav_msgs::OccupancyGrid::ConstPtr &msg)
+{
+    // perform distance transform to build the distance field
+    mapWidth_ = msg->info.width;
+    mapHeight_ = msg->info.height;
+    mapResolution_ = msg->info.resolution;
+    cv::Mat binMap(mapHeight_, mapWidth_, CV_8UC1);
+    for (int v = 0; v < mapHeight_; v++)
+    {
+        for (int u = 0; u < mapWidth_; u++)
+        {
+            int node = v * mapWidth_ + u;
+            int val = msg->data[node];
+            if (val == 100)
+                binMap.at<uchar>(v, u) = 0;
+            else
+                binMap.at<uchar>(v, u) = 1;
+        }
+    }
+    cv::Mat distMap(mapHeight_, mapWidth_, CV_32FC1);
+    cv::distanceTransform(binMap, distMap, cv::DIST_L2, 5);
+    for (int v = 0; v < mapHeight_; v++)
+    {
+        for (int u = 0; u < mapWidth_; u++)
+        {
+            float d = distMap.at<float>(v, u) * (float)mapResolution_;
+            distMap.at<float>(v, u) = d;
+        }
+    }
+    distMap_ = distMap;
+    tf2::Quaternion q(msg->info.origin.orientation.x,
+                     msg->info.origin.orientation.y,
+                     msg->info.origin.orientation.z,
+                     msg->info.origin.orientation.w);
+    double roll, pitch, yaw;
+    tf2::Matrix3x3 m(q);
+    m.getRPY(roll, pitch, yaw);
+    mapOrigin_.setX(msg->info.origin.position.x);
+    mapOrigin_.setY(msg->info.origin.position.y);
+    mapOrigin_.setYaw(yaw);
+    gotMap_ = true;
+}
