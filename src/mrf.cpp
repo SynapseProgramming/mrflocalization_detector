50f337e64137e0ef4779ce7c51097fcacb58aca1
Roald Ong <roald.ong@gmail.com>
2024-01-22 16:17:59 +0800

    Add in translation code to translate points to map frame, and marker visualizations

--- src/mrf.cpp
+++ src/mrf.cpp
@@ -4,6 +4,57 @@ void MRF::scanCB(const sensor_msgs::LaserScan::ConstPtr &msg)
 {
 
     std::cout << "scan message received!\n";
+
+    // plot point_out in rviz for testing
+    visualization_msgs::Marker marker;
+    marker.header.frame_id = mapFrameName;
+    marker.header.stamp = ros::Time::now();
+    marker.ns = "laser_points";
+    marker.id = 0;
+    marker.type = visualization_msgs::Marker::POINTS;
+    marker.action = visualization_msgs::Marker::ADD;
+    marker.pose.orientation.w = 1.0;
+    marker.scale.x = 0.1;
+    marker.scale.y = 0.1;
+    marker.color.r = 1.0;
+    marker.color.a = 1.0;
+
+    std::vector<std::pair<double, double>> laserPoints;
+    for (int i = 0; i < msg->ranges.size(); i++)
+    {
+        double d = (double)i * msg->angle_increment + msg->angle_min;
+        double xx = msg->ranges[i] * cos(d);
+        double yy = msg->ranges[i] * sin(d);
+        if (xx >= -100 && xx <= 100 && yy >= -100 && yy <= 100)
+            laserPoints.push_back(std::make_pair(xx, yy));
+    }
+    // translate laser points to be wrt to map frame
+    geometry_msgs::Point point;
+    for (int i = 0; i < laserPoints.size(); i++)
+    {
+        auto it = laserPoints[i];
+        geometry_msgs::PointStamped point_in;
+        geometry_msgs::PointStamped point_out;
+        point_in.header.frame_id = scanFrameName;
+        point_in.point.x = it.first;
+        point_in.point.y = it.second;
+        try
+        {
+            point_out = tfBuffer.transform(point_in, mapFrameName);
+        }
+        catch (tf2::TransformException &ex)
+        {
+            ROS_WARN("%s", ex.what());
+            return;
+        }
+
+        point = point_out.point;
+        if (i % 10 == 0)
+            marker.points.push_back(point);
+    }
+
+    markerPub_.publish(marker);
+
     gotScan_ = true;
 }
 
@@ -16,6 +67,8 @@ void MRF::mapCB(const nav_msgs::OccupancyGrid::ConstPtr &msg)
     mapWidth_ = msg->info.width;
     mapHeight_ = msg->info.height;
     mapResolution_ = msg->info.resolution;
+    std::cout << "map width: " << mapWidth_ << "\n";
+    std::cout << "map height: " << mapHeight_ << "\n";
     cv::Mat binMap(mapHeight_, mapWidth_, CV_8UC1);
     for (int v = 0; v < mapHeight_; v++)
     {
@@ -50,5 +103,8 @@ void MRF::mapCB(const nav_msgs::OccupancyGrid::ConstPtr &msg)
     mapOrigin_.setX(msg->info.origin.position.x);
     mapOrigin_.setY(msg->info.origin.position.y);
     mapOrigin_.setYaw(yaw);
+    std::cout << "origin x: " << mapOrigin_.getX() << "\n";
+    std::cout << "origin y: " << mapOrigin_.getY() << "\n";
+    std::cout << "origin yaw: " << mapOrigin_.getYaw() << "\n";
     gotMap_ = true;
 }
